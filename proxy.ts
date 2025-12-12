949c664d60ffe25f0dff54e9328f3561655da5bd
Goduu <idcangussu@hotmail.com>
2025-12-12 20:44:18 +0100

    update proxy

--- proxy.ts
+++ proxy.ts
@@ -1,6 +1,6 @@
 import { NextResponse } from 'next/server';
 import type { NextRequest } from 'next/server';
-import { locales, getLocale } from './i18n/locale';
+import { locales, getLocale } from './src/i18n/locale';
 
 export function proxy(request: NextRequest) {
     const { pathname } = request.nextUrl;
